a683a68fe1f9e85b794b9635656fd3fbac8f2357
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2024-05-24 08:12:44 +0530

    vpc-test

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "public_subnet_cidrs" {
+    default = ["10.0.1.0/24","10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs"{
+    default = ["10.0.4.0/24","10.0.5.0/24"]
+}
+
+variable "database_subnet_cidrs" {
+    default = ["10.0.6.0/24","10.0.7.0/24"]
+}
+
+variable "is_peering_required" {
+    type = bool
+    default = true
+}
